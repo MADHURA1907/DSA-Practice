9cf9ffbea7a1ed5f2ef05f22c23853be4e8fca55
MADHURA1907 <barvemadhura19@gmail.com>
2026-04-05 20:59:32 +0530

    Add new leetcode problem

---
+++ Math/2396-Strictly-Palindromic-Number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+        
+    }
+};
+
+int main()
+{
+    Solution s;
+    int n=9;
+    cout<<s.isStrictlyPalindromic(n);
+    return 0;
+}
