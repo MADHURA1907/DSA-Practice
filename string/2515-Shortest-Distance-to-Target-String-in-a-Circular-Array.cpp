68d221edbde63267fd964244a13fa29d25758ba2
MADHURA1907 <barvemadhura19@gmail.com>
2026-04-15 21:39:31 +0530

    new leetcode problem

---
+++ string/2515-Shortest-Distance-to-Target-String-in-a-Circular-Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        int n = words.size();
+        int ans = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+            if(words[i] == target) {
+                int diff = abs(i - startIndex);
+                int dist = min(diff, n - diff);
+                ans = min(ans, dist);
+            }
+        }
+
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<string> words = {"hello", "i", "am", "leetcode", "hello"};
+    string target = "hello";
+    int startIndex = 1;
+    cout << s.closestTarget(words, target, startIndex) << endl;
+    return 0;
+
+}
