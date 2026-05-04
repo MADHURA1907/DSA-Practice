d0c2356e5eca3909cc969eba65758696d3cfcfd9
MADHURA1907 <barvemadhura19@gmail.com>
2026-05-04 18:41:18 +0530

    New leetcode problem

---
+++ bfs_dfs/104-Maximum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ }; 
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+        {
+            return 0;
+        }
+
+        int l=maxDepth(root->left);
+        int r=maxDepth(root->right);
+        return 1+(max(l,r));
+        
+    }
+};
+
+
+int main()
+{
+    TreeNode* root=new TreeNode(1);
+    root->left=new TreeNode(2);
+    root->right=new TreeNode(3);
+    root->left->left=new TreeNode(4);
+    root->left->right=new TreeNode(5);
+
+    Solution s;
+    cout<<s.maxDepth(root)<<endl;
+
+    return 0;
+}
