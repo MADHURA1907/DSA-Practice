21f01dbb76e48da283264400af5808a3dcae7828
MADHURA1907 <barvemadhura19@gmail.com>
2026-06-05 09:08:28 +0530

    New leetcode problem

---
+++ Arrays/350-Intersection-of-Two-Arrays-II.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size())
+        {
+            if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+            else if(nums1[i]>nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                result.push_back(nums1[i]);
+                i++;j++;
+            }
+        }
+
+        return result;
+        
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums1={1,2,2,1};
+    vector<int> nums2={2,2};
+    vector<int> result=s.intersect(nums1,nums2);
+    for(int i=0;i<result.size();i++)
+    {
+        cout<<result[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
