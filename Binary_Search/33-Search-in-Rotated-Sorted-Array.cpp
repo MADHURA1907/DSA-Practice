08b20e2ddda3ca46196e607668058a770739e2df
MADHURA1907 <barvemadhura19@gmail.com>
2026-06-19 19:28:27 +0530

    New leetcode problem

---
+++ Binary_Search/33-Search-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            // Left half sorted
+            if(nums[low] <= nums[mid])
+            {
+                if(target >= nums[low] && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            // Right half sorted
+            else
+            {
+                if(target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
+    cout << obj.search(nums, target) << endl;
+
+    return 0;
+}
