964a096718acd4035871c0559b22e1e3c8af80aa
MADHURA1907 <barvemadhura19@gmail.com>
2026-07-30 21:10:31 +0530

    Add solution for LeetCode 540

---
+++ Binary_Search/540-Single-Element-in-a-Sorted-Array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Make mid even
+            if (mid % 2 == 1)
+                mid--;
+
+            if (nums[mid] == nums[mid + 1])
+                low = mid + 2;
+            else
+                high = mid;
+        }
+
+        return nums[low];
+    }
+};
+
+int main()
+{
+    vector<int> nums={1,1,2,3,3,4,4,8,8};
+    Solution s;
+    s.singleNonDuplicate(nums);
+}
