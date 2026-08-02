9038660b5dc448f7ea4b4006684e98aebed7ea5e
MADHURA1907 <barvemadhura19@gmail.com>
2026-08-02 23:38:36 +0530

    New leetcode Problem

---
+++ Math/3658.-GCD-of-Odd-and-Even-Sums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout<<s.gcdOfOddEvenSums(10)<<endl;
+    return 0;
+}
