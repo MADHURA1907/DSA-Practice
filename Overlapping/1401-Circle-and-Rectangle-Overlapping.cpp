38b08a173e141f250a593e97d5918cd1a5a2eabc
MADHURA1907 <barvemadhura19@gmail.com>
2026-09-19 20:41:27 +0530

    New leetcode Problem

---
+++ Overlapping/1401-Circle-and-Rectangle-Overlapping.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter,int x1, int y1, int x2, int y2) {
+
+        int closestX = max(x1, min(xCenter, x2));
+        int closestY = max(y1, min(yCenter, y2));
+
+        int dx = xCenter - closestX;
+        int dy = yCenter - closestY;
+
+        return dx * dx + dy * dy <= radius * radius;
+    }
+};
+
+int main() {
+    Solution solution;
+    int radius = 1;
+    int xCenter = 0;
+    int yCenter = 0;
+    int x1 = -1;
+    int y1 = -1;
+    int x2 = 1;
+    int y2 = 1;
+
+    bool result = solution.checkOverlap(radius, xCenter, yCenter, x1, y1, x2, y2);
+    cout << (result ? "true" : "false") << endl;
+
+    return 0;
+}
